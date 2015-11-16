16698adc7ea88c8b695ab5f6dc2a3d64041bc61d
Zachary Smith <billyzacsmith@gmail.com>
2015-11-16 13:58:35 -0700

    Improve organization and syntax in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,36 @@
 var gulp = require('gulp')
 var mocha = require('gulp-mocha')
 
-gulp.task('default', ['test', 'build html', 'build js', 'build css'], function() {
-})
+gulp.task('default', ['build'])
+
+gulp.task('build', ['build:html', 'build:js', 'build:css'])
 
 gulp.task('test', function() {
   return gulp.src('./test/*.js', {read: false})
     .pipe(mocha())
 })
 
-gulp.task('build html', function() {
+gulp.task('build:html', function() {
   return gulp
     .src('./src/**/*.html')
     .pipe(gulp.dest('./build'))
 })
 
-gulp.task('build js', function() {
+gulp.task('build:js', ['test'], function() {
   return gulp
     .src('./src/**/*.js')
     .pipe(gulp.dest('./build'))
 })
 
-gulp.task('build css', function() {
+gulp.task('build:css', function() {
   return gulp
     .src('./src/**/*.css')
     .pipe(gulp.dest('./build'))
 })
 
 gulp.task('watch', ['default'], function () {
-    gulp.watch('./src/**/*.html', ['default'])
-    gulp.watch('./src/**/*.css', ['default'])
-    gulp.watch('./src/**/*.js', ['default'])
+    gulp.watch('./src/**/*.html', ['build:html'])
+    gulp.watch('./src/**/*.css', ['build:css'])
+    gulp.watch('./src/**/*.js', ['build:js'])
     gulp.watch('./test/*.js', ['test'])
 })
