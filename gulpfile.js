33943b5fc94fb943fecdea8d062396edfd66af4e
Zachary Smith <billyzacsmith@gmail.com>
2015-11-16 11:57:48 -0700

    Add the building of the js and css to the gulp workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp')
 var mocha = require('gulp-mocha')
 
-gulp.task('default', ['test'], function() {
-  gulp
-    .src('./src/**/*.html')
-    .pipe(gulp.dest('./build'))
+gulp.task('default', ['test', 'build html', 'build js', 'build css'], function() {
 })
 
 gulp.task('test', function() {
   return gulp.src('./test/*.js', {read: false})
     .pipe(mocha())
+})
+
+gulp.task('build html', function() {
+  return gulp
+    .src('./src/**/*.html')
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('build js', function() {
+  return gulp
+    .src('./src/**/*.js')
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('build css', function() {
+  return gulp
+    .src('./src/**/*.css')
+    .pipe(gulp.dest('./build'))
 })
