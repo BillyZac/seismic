065f5b8e02271adbef89255e42b8d537311d06cf
Zachary Smith <billyzacsmith@gmail.com>
2015-11-16 14:19:48 -0700

    Set up basic Chai Mocha test framework.

---
+++ test/test.js
@@ -0,0 +1,10 @@
+// var code = require('../src/js') /* where should this point */
+var expect = require('chai').expect
+
+// Do I need the line in the html?
+
+describe('Earthquake app', function() {
+  it('whenever whatever happens', function() {
+    expect(undefined).to.be.undefined
+  })
+})
