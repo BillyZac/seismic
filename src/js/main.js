fdfea59e09f614c094975f89d5e54bbced675e32
Zachary Smith <billyzacsmith@gmail.com>
2015-11-17 16:50:14 -0700

    A simple function that demonstrates putting an element in the DOM. Wrapped all the js in $(document).ready().

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,32 @@
 var $ = require('jquery')
+  $(document).ready(function() {
+  function sanityCheck(a, b) {
+    // console.log(a, b)
+    return a + b
+  }
+  sanityCheck(1,2)
 
-function sanityCheck(a, b) {
-  // console.log(a, b)
-  return a + b
+  var url = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
+  function connect(url) {
+    $.get(url)
+      .done(function(data) {
+        var quakes = data.features
+        for (var i=0; i<5; i++) {
+          console.log(data.features[i].properties.title)
+          var title = data.features[i].properties.title
+          $('.map').append('<p>' + title + '</p>' )
+        }
+      })
+  }
+  // connect(url)
+
+  drawSomething('foo')
+
+  function drawSomething(title) {
+      $('.foo').append('<p>Bar</p>')
+  }
+
+  module.exports.sanityCheck = sanityCheck
+  module.exports.connect = connect
+  module.exports.drawSomething = drawSomething
 }
-sanityCheck(1,2)
-
-var url = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
-function connect(url) {
-  $.get(url)
-    .done(function(data) {
-      var quakes = data.features
-      for (var i=0; i<5; i++) {
-        console.log(data.features[i].properties.title)
-        var title = data.features[i].properties.title
-        $('.map').append('<p>' + title + '</p>' )
-      }
-    })
-}
-connect(url)
-
-
-module.exports.sanityCheck = sanityCheck
-module.exports.connect = connect
