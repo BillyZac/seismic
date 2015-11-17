c4dd05b2197f4e141d5eef9386df424baf12ed5a
Zachary Smith <billyzacsmith@gmail.com>
2015-11-17 11:10:51 -0700

    Send get request to the USGS API. Show a list of earthquake titles using console.log.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,23 @@
 var $ = require('jquery')
 
 function sanityCheck(a, b) {
-  console.log(a, b)
+  // console.log(a, b)
   return a + b
 }
 sanityCheck(1,2)
+
+var url = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
+function connect(url) {
+  $.get(url)
+    .done(function(data) {
+      var quakes = data.features
+      for (var i=0; i<quakes.length; i++) {
+        console.log(data.features[i].properties.title)
+      }
+    })
+}
+connect(url)
+
+
 module.exports.sanityCheck = sanityCheck
+module.exports.connect = connect
