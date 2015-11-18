97451109facb1f29276fd154af9f9dafba9f7fd6
Zachary Smith <billyzacsmith@gmail.com>
2015-11-17 20:27:35 -0700

    Draw a circle to the page using SVG and jQuery. The little function SVG creates an svg element. Since the $() function accepts a DOM element, we can then wrap the SVG() with $(). This allows us to apply jQuery methods to the svg, especially .attr and .appendTo.

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,43 @@
 var $ = require('jquery')
-  $(document).ready(function() {
-  function sanityCheck(a, b) {
-    // console.log(a, b)
-    return a + b
-  }
-  sanityCheck(1,2)
 
-  var url = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
-  function connect(url) {
-    $.get(url)
-      .done(function(data) {
-        var quakes = data.features
-        for (var i=0; i<5; i++) {
-          console.log(data.features[i].properties.title)
-          var title = data.features[i].properties.title
-          $('.map').append('<p>' + title + '</p>' )
-        }
-      })
-  }
-  // connect(url)
-
-  drawSomething('foo')
-
-  function drawSomething(title) {
-      $('.foo').append('<p>Bar</p>')
-  }
-
-  module.exports.sanityCheck = sanityCheck
-  module.exports.connect = connect
-  module.exports.drawSomething = drawSomething
+function sanityCheck(a, b) {
+  // console.log(a, b)
+  return a + b
 }
+sanityCheck(1,2)
+
+var url = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
+function connect(url) {
+  $.get(url)
+    .done(function(data) {
+      var quakes = data.features
+      for (var i=0; i<5; i++) {
+        console.log(data.features[i].properties.title)
+        var title = data.features[i].properties.title
+        $('.map').append('<p>' + title + '</p>' )
+      }
+    })
+}
+// connect(url)
+
+drawCircle()
+
+function drawCircle() {
+  var $svg = $('#mysvg');
+  $(SVG('circle'))
+    .attr('cx', 130)
+    .attr('cy', 75)
+    .attr('r', 50)
+    .attr('fill', 'none')
+    .attr('stroke', 'red')
+    .attr('stroke-width', 3)
+    .appendTo($svg);
+}
+
+function SVG(tag) {
+   return document.createElementNS('http://www.w3.org/2000/svg', tag);
+}
+
+module.exports.sanityCheck = sanityCheck
+module.exports.connect = connect
+module.exports.drawSomething = drawCircle
