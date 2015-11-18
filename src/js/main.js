8c73dc37121b9c508ce8ed37f85cfdd20f239607
Zachary Smith <billyzacsmith@gmail.com>
2015-11-18 12:29:46 -0700

    Show info about the earthquake event when hovering over a circle.
    Add data attributes to the circles for magnitude, time, and place. Also add a data attribute called data-type: point, which is used to select the circle.
    
    Create the basic html elements for the info box, which serves as containers for the place and magnitude.

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,7 @@ function connect(url) {
   $.get(url)
     .done(function(data) {
       var quakes = data.features
+      // console.log(quakes)
       for (var i=0; i<quakes.length; i++) {
         var title = quakes[i].properties.title
         // $('body').append('<p>' + title + '</p>' )
@@ -18,13 +19,22 @@ function connect(url) {
           x: quakes[i].geometry.coordinates[0] * 8,
           y: quakes[i].geometry.coordinates[1] * 8,
           radius: quakes[i].properties.sig * 0.07,
-          mag: quakes[i].properties.mag
+          mag: quakes[i].properties.mag,
+          place: quakes[i].properties.place,
+          time: quakes[i].properties.time
         }
-        if (dataPoint.mag > 4) {
-          console.log(dataPoint)
+        if (dataPoint.mag > 5) {
+          // console.log(dataPoint)
           drawCircle(dataPoint)
         }
       }
+      // Listen for hover over circles. Show more info about the event when hovering.
+      $( "circle[data-type='point']" ).hover( function() {
+        $mag = $(this).attr('data-mag')
+        $('.mag').text($mag)
+        $place = $(this).attr('data-place')
+        $('.place').text($place)
+      })
     })
 }
 connect(url)
@@ -38,7 +48,10 @@ function drawCircle(dataPoint) {
     .attr('fill', 'aqua')
     .attr('stroke', 'none')
     .attr('opacity', 0.2)
-    .attr('stroke-width', 1)
+    .attr('stroke-width', 3)
+    .attr('data-type', 'point')
+    .attr('data-mag', dataPoint.mag)
+    .attr('data-place', dataPoint.place)
     .appendTo($svg)
 }
 
