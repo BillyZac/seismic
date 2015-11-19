4889083acd91c38320f69f592c1a0fbe75e7bab8
Zachary Smith <billyzacsmith@gmail.com>
2015-11-19 08:47:55 -0700

    Convert latitude and longitude to screen coordinates.
    The global map image, which is from [Natural Earth](www.naturalearthdata.com), uses the Geographic coordinate system. The conversion formula converts from the Mercator system. I'm not clear the difference between the two projection systems, but the points seem to land in the right place, at least according to my initial tests.
    
    [Source of conversion formula](http://stackoverflow.com/questions/14329691/covert-latitude-longitude-point-to-a-pixels-x-y-on-mercator-projection/14457180#14457180)
    [Source of map image](http://www.naturalearthdata.com/downloads/50m-gray-earth/gray-earth-with-ocean-bottom/)

--- src/js/main.js
+++ src/js/main.js
@@ -37,7 +37,7 @@ function connect(url) {
       })
     })
 }
-connect(url)
+// connect(url)
 
 function drawCircle(dataPoint) {
   var $svg = $('.map');
@@ -58,6 +58,81 @@ function drawCircle(dataPoint) {
 function SVG(tag) {
    return document.createElementNS('http://www.w3.org/2000/svg', tag);
 }
+
+// Testing the plot accuracy
+// 39.7642548,-104.9951955
+var denver = {
+  x: 240,
+  y: 160,
+  radius: 40,
+  mag: 666,
+  place: 'Denver',
+  time: 'forever'
+}
+
+// -37.8589546,144.5191771
+var melbourne = {
+  x: 1075,
+  y: 420,
+  radius: 40,
+  mag: 666,
+  place: 'Melbourse',
+  time: 'forever'
+}
+var zero = {
+  x: 0,
+  y: 0,
+  radius: 30
+}
+var max = {
+  x: 1190,
+  y: 595,
+  radius: 30
+}
+
+drawCircle(denver)
+drawCircle(melbourne)
+drawCircle(zero)
+drawCircle(max)
+
+var mapWidth    = 1190
+var mapHeight   = 595
+function convert(latitude, longitude) {
+  // From: http://stackoverflow.com/questions/14329691/covert-latitude-longitude-point-to-a-pixels-x-y-on-mercator-projection/14457180#14457180
+  var x = ( longitude + 180 ) * ( mapWidth / 360 )
+
+  // convert from degrees to radians
+  var latRad = latitude * Math.PI / 180
+
+  // get y value
+  var mercN = Math.log(Math.tan((Math.PI/4)+(latRad/2)))
+  var y     = (mapHeight/2)-(mapWidth*mercN/(2*Math.PI))
+
+  return [x,y]
+}
+console.log('melbourne -- Expect x: 1075, y: 420', convert(-37.8589546, 144.5191771))
+console.log(convert(39.7642548, -104.9951955))
+
+function convertLongitude(longitude) {
+  var x = ( longitude + 180 ) * ( mapWidth / 360 )
+  return x
+}
+// Test convertLongitude()
+console.log('Expect convertLongitude(144.5191771) --> 1075: ', convertLongitude(144.5191771))
+console.log('Expect convertLongitude(-104.9951955) --> 240: ', convertLongitude(-104.9951955))
+
+function convertLatitude(latitude) {
+  // convert from degrees to radians
+  var latRad = latitude * Math.PI / 180
+
+  var mercN = Math.log(Math.tan((Math.PI/4)+(latRad/2)))
+  var y     = (mapHeight/2)-(mapWidth*mercN/(2*Math.PI))
+
+  return y
+}
+console.log('Expect convertLatitude(-37.8589546) --> 420: ', convertLatitude(-37.8589546))
+console.log('Expect convertLatitude(39.7642548) --> 160: ', convertLatitude(39.7642548))
+
 
 module.exports.sanityCheck = sanityCheck
 module.exports.connect = connect
