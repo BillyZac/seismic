19c0ca3b0ada80a9a366ef424ad15f7884878959
Zachary Smith <billyzacsmith@gmail.com>
2015-11-19 15:06:30 -0700

    Is the USGS API offline? Handle it.
    Upon successful connection to the API, store the response in the browser's local storage. Also store a time stamp with the data.
    
    If the API is offline, and the user has visited the page before, the data is pulled from local storage and the earthquakes are still displayed.
    
    Show the user the time stamp of the data. Notify the user when the API is offline, and when the data is pulled from the cached data.

--- src/js/main.js
+++ src/js/main.js
@@ -13,14 +13,27 @@ sanityCheck(1,2)
 
 var url = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
 function connect(url) {
+  var quakesCollection = {
+    timeCollected: 'never',
+    list: []
+  }
+
+  if (localStorage.getItem('quakesCollection')) {
+    // Get the quakesCollection that was stored in the browser's localStorage on a previous page load
+    quakesCollection = JSON.parse(localStorage.getItem('quakesCollection'))
+    // Get the timestamp of the cached data, and convert it to a Date object
+    var cachedTimeCollected = new Date(quakesCollection.timeCollected)
+    // Update with quakesCollection object with the timestamp of the cached data
+    quakesCollection.timeCollected = cachedTimeCollected
+  }
+
   $.get(url)
     .done(function(data) {
+      console.log('Successfully connected')
+      // Timestamp the quakesCollection
+      quakesCollection.timeCollected = new Date()
+      console.log('Newly created time stamp:', quakesCollection.timeCollected)
       var quakes = data.features
-      var quakesCollection = {
-        timeCollected: 0,
-        list: []
-      }
-
       for (var i=0; i<quakes.length; i++) {
         var title = quakes[i].properties.title
         // $('body').append('<p>' + title + '</p>' )
@@ -32,17 +45,37 @@ function connect(url) {
           place: quakes[i].properties.place,
           time: quakes[i].properties.time
         }
-
         // Convert map coordinates to screen coordinates
         dataPoint.x =  Math.floor(convertLongitude(dataPoint.longitude))
         dataPoint.y =  Math.floor(convertLatitude(dataPoint.latitude))
-
         // Put the quake in the quakesCollection
         quakesCollection.list.push(dataPoint)
       }
+      localStorage.clear()
+      // localStorage.setItem('quakesCollection', JSON.stringify(quakesCollection))
 
+    })
+    .error(function() {
+      console.log('Could not connect to USGS.')
+      if (localStorage.getItem('quakesCollection')) {
+        quakesCollection = JSON.parse(localStorage.getItem('quakesCollection'))
+      }
+    })
+    .always(function(APIresponse) {
+      // Show notification if cannot connect to API
+      if (APIresponse.statusText === 'error') {
+        if (quakesCollection.timeCollected === 'never') {
+          $('.notifications').text('Could not connect to USGS.')
+        } else {
+          var cachedTimeCollected = new Date(quakesCollection.timeCollected)
+          $('.notifications').text('Could not connect to USGS. Data last collected at '
+          + cachedTimeCollected.toLocaleString())
+        }
+      } else {
+        $('.notifications').text('Data collected at ' + quakesCollection.timeCollected.toLocaleString())
+      }
       // Set the maximum magnitude of quakes to draw
-      var maxMagnitude = 5.5
+      var maxMagnitude = 6.0
       // Draw all quakes above the specified magnitude
       render.drawAll(quakesCollection, maxMagnitude)
 
@@ -53,9 +86,6 @@ function connect(url) {
         $place = $(this).attr('data-place')
         $('.place').text($place)
       })
-    })
-    .error(function() {
-      console.log('Could not connect to USGS.')
     })
 }
 connect(url)
