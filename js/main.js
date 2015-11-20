9bb851f3ae74ff1e6cf7f0c72c04375bcdafe062
Zachary Smith <billyzacsmith@gmail.com>
2015-11-20 04:17:10 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -86,8 +86,6 @@
 	      // Timestamp the quakesCollection
 	      quakesCollection.timeCollected = new Date()
 	      var quakes = data.features
-	      console.log(quakes)
-	      console.log('quakes', quakes.length)
 	      for (var i=0; i<quakes.length; i++) {
 	        var title = quakes[i].properties.title
 	        // $('body').append('<p>' + title + '</p>' )
@@ -143,12 +141,16 @@
 	        $('.date').text( new Date($time).toLocaleDateString())
 	        $('.time').text( new Date($time).toLocaleTimeString())
 	      })
+	
+	      $('circle').click( function() {
+	        photos.clearPhotos()
+	        photos.photos($(this).attr('latitude'), $(this).attr('longitude'))
+	      })
 	    })
 	}
 	connect(url)
 	
 	$('body').mousemove(function(event) {
-	  console.log(event.pageY)
 	  if (event.pageY < 20) {
 	    $('header').slideDown(50)
 	  }
@@ -9416,6 +9418,8 @@
 	  $(SVG('circle'))
 	    .attr('cx', dataPoint.x)
 	    .attr('cy', dataPoint.y)
+	    .attr('latitude', dataPoint.latitude)
+	    .attr('longitude', dataPoint.longitude)
 	    .attr('r', dataPoint.radius)
 	    .attr('fill', 'aqua')
 	    .attr('stroke', 'none')
@@ -9441,19 +9445,68 @@
 	
 	function photos(latitude, longitude) {
 	  // get photos associated with the latitude and longitude
+	  console.log(latitude, longitude)
+	    var flickerAPI = 'https://api.flickr.com/services/rest/'
+	      + '?method=flickr.photos.search'
+	      + '&api_key=439d3c788d4c522138a62a1a9faf2f10'
+	      // + '&tags=earthquake%2C+earthquakes'
+	      + '&sort=interestingness-desc'
+	      + '&media=photos'
+	      + '&lat=' + latitude
+	      + '&lon=' + longitude
+	      + '&radius=32&radius_units=km'
+	      + '&per_page=9&page=1'
+	      + '&format=json&nojsoncallback=1'
+	    function displayPhotos(data) {
+	      console.log(data)
+	      if (data.photos) {
+	        var photoHTML = '<ul>'
+	        $.each(data.photos.photo, function(i,photo) {
+	          var imgSrc = 'https://farm'
+	            + photo.farm
+	            + '.staticflickr.com/'
+	            + photo.server
+	            + '/'
+	            + photo.id
+	            + '_'
+	            + photo.secret
+	            + '.jpg'
+	
+	          photoHTML += '<li><a href="'
+	            + '#'
+	            + '">'
+	            + '<img src="'
+	            + imgSrc
+	            + '"></a></li>'
+	        })
+	        photoHTML += '</ul>'
+	        $('.photos').html(photoHTML)
+	      }
+	    }
+	    $.getJSON(flickerAPI, displayPhotos);
 	    // Put lat and long into a flickr search
 	    // Receive a list of photos
 	    // Push up to 12 photo urls into an array photoCollection
 	    // Append photos to sidebar
+	  // $('.photos').html('').hide()
+	  // var photoCollection =
+	  //   ['http://images.usatoday.com/news/_photos/2005/10/08/quake-colombia.jpg',
+	  //    'http://images-thumbs.thefullwiki.org/1/9/0/1906_San_Francisco_earthquake.png',
+	  //    'http://nisee.berkeley.edu/thumbnail/3046_1033_1323/IMG0019.jpg',
+	  //    'http://news.nationalgeographic.com/news/images/thumbs/060413_earthquake_170.jpg']
+	  // for (var i=0; i<photoCollection.length; i++) {
+	  //   var newImage = '<img src="' + photoCollection[i] + '"></img>'
+	  //   $('.photos').append(newImage)
+	  // }
+	  // $('.photos').fadeIn(200)
+	}
+	
+	function clearPhotos() {
 	  $('.photos').html('')
-	  var photoCollection = ['url1', 'url2', 'url3']
-	  for (var i=0; i<photoCollection.length; i++) {
-	    var newImage = '<img src="' + photoCollection[i] + '"></img>'
-	    $('.photos').append(newImage)
-	  }
 	}
 	
 	module.exports.photos = photos
+	module.exports.clearPhotos = clearPhotos
 
 
 /***/ }
